66a84f6b4afc367c3536bad5afbc5a0028a54225
real <real@freedomlayer.org>
2017-08-15 14:36:13 +0300

    Added EchoRev, and using it in main.

--- echo_proto/src/main.rs
+++ echo_proto/src/main.rs
@@ -4,12 +4,12 @@ extern crate tokio_proto;
 use tokio_proto::TcpServer;
 
 use echo_proto::line_proto::LineProto;
-use echo_proto::service::Echo;
+use echo_proto::service::{/*Echo,*/ EchoRev};
 
 fn main() {
     let addr = "0.0.0.0:12345".parse().unwrap();
     let server = TcpServer::new(LineProto, addr);
 
     println!("Server is listening on port 12345...");
-    server.serve(|| Ok(Echo));
+    server.serve(|| Ok(EchoRev));
 }
--- echo_proto/src/service.rs
+++ echo_proto/src/service.rs
@@ -18,3 +18,21 @@ impl Service for Echo {
         future::ok(req).boxed()
     }
 }
+
+pub struct EchoRev;
+
+impl Service for EchoRev {
+    type Request = String;
+    type Response = String;
+
+    type Error = io::Error;
+    type Future = BoxFuture<Self::Response, Self::Error>;
+
+    fn call(&self, req: Self::Request) -> Self::Future {
+        // Reversing characters here:
+        let rev: String = req.chars()
+            .rev()
+            .collect();
+        future::ok(rev).boxed()
+    }
+}
