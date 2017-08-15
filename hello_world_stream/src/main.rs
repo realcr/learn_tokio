5bbc993d15dfaebedf1090d7ef46dea786ac107a
real <real@freedomlayer.org>
2017-08-15 18:56:00 +0300

    Added concurrent server example.

--- hello_world_stream/src/main.rs
+++ hello_world_stream/src/main.rs
@@ -3,10 +3,12 @@ extern crate tokio_core;
 extern crate tokio_io;
 
 use futures::stream::Stream;
+use futures::Future;
 use tokio_core::reactor::Core;
 use tokio_core::net::TcpListener;
 
-fn main() {
+#[allow(dead_code)]
+fn sequential_server()  {
     let mut core = Core::new().unwrap();
     let address = "0.0.0.0:12345".parse().unwrap();
     let listener = TcpListener::bind(&address, &core.handle())
@@ -21,6 +23,35 @@ fn main() {
         Ok(())
     });
 
-    println!("Listening on port 12345...");
     core.run(server).unwrap()
+}
+
+#[allow(dead_code)]
+fn concurrent_server() {
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+
+
+    let address = "0.0.0.0:12345".parse().unwrap();
+    let listener = TcpListener::bind(&address, &core.handle())
+        .unwrap();
+
+    let connections = listener.incoming();
+
+    let server = connections.for_each(|(socket, _peer_addr)| {
+        let serve_one = tokio_io::io::write_all(socket, b"Hello, world!\n")
+            .then(|_| Ok(()));
+        handle.spawn(serve_one);
+        Ok(())
+    });
+
+    core.run(server).unwrap()
+
+}
+
+fn main() {
+
+    println!("Listening on port 12345...");
+    // sequential_server();
+    concurrent_server();
 }
