0bfc0ba86807d2b242986dc5b1bc6d326fd0adf1
real <real@freedomlayer.org>
2017-08-16 13:05:14 +0300

    Using our serve, instead of TcpServer's send.

--- pipelined_server/src/main.rs
+++ pipelined_server/src/main.rs
@@ -1,15 +1,17 @@
 extern crate pipelined_server;
 extern crate tokio_proto;
 
-use tokio_proto::TcpServer;
+// use tokio_proto::TcpServer;
 
-use pipelined_server::line_proto::LineProto;
+// use pipelined_server::line_proto::LineProto;
 use pipelined_server::service::{/*Echo,*/ EchoRev};
 
+use pipelined_server::serve;
+
 fn main() {
-    let addr = "0.0.0.0:12345".parse().unwrap();
-    let server = TcpServer::new(LineProto, addr);
+    // let addr = "0.0.0.0:12345".parse().unwrap();
+    // let server = TcpServer::new(LineProto, addr);
 
     println!("Server is listening on port 12345...");
-    server.serve(|| Ok(EchoRev));
+    let _ = serve(|| Ok(EchoRev), "0.0.0.0:12345");
 }
