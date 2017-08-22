e14726099922e72fdcdb88925527e2900d3d3b60
real <real@freedomlayer.org>
2017-08-22 16:02:43 +0300

    Added a message for server listening.

--- streaming_proto/src/main.rs
+++ streaming_proto/src/main.rs
@@ -9,5 +9,6 @@ use streaming_proto::service::PrintStdout;
 fn main() {
     let addr = "0.0.0.0:12345".parse().unwrap();
     let server = TcpServer::new(LineProto, addr);
+    println!("Listening on port 12345 ...");
     server.serve(|| Ok(PrintStdout));
 }
